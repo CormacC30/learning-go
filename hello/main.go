23df0453753a143f6dd0a2b0a4876e95b9bf06cb
Cormac Costello <ccostell@ccostell-thinkpadt14gen4.remote.csb>
2024-11-29 08:48:52 +0000

    time tutorial

--- hello/main.go
+++ hello/main.go
@@ -4,6 +4,32 @@ import (
 	"fmt"
 )
 
+const aConst int = 64
+
 func main() {
-	fmt.Println("Hello from Go!")
+
+	var aString string = "This is Go!"
+	fmt.Println(aString)
+	fmt.Printf("This variable's type is %T\n", aString)
+
+	var anInteger int = 42
+	fmt.Println(anInteger)
+
+	var defaultInt int
+	fmt.Println(defaultInt)
+
+	var anotherString = "THis is another string"
+	fmt.Println(anotherString)
+	fmt.Printf("This variable's type is %T\n", anotherString)
+
+	var anotherInt int = 15
+	fmt.Println(anotherInt)
+	fmt.Printf("This variable's type is %T\n", anotherInt)
+
+	myString := "This is also a string"
+	fmt.Println(myString)
+	fmt.Printf("This variable's type is %T\n", myString)
+
+	fmt.Println(aConst)
+	fmt.Printf("This variable's type is %T\n", aConst)
 }
